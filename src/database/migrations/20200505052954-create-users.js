a88e37cdf7e8d63755c4799d44c68a67327f09a7
leoventura-30 <leonardo.ventura@outlook.com>
2020-11-04 22:51:57 -0300

    chore: init

--- src/database/migrations/20200505052954-create-users.js
+++ src/database/migrations/20200505052954-create-users.js
@@ -2,13 +2,6 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
     return queryInterface.createTable("users", {
       id: {
         allowNull: false,
