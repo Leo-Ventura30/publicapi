f5577a0ae89f57cdba266e4d364cadd84c31a5a5
leoventura-30 <leonardo.ventura@outlook.com>
2020-11-04 22:50:40 -0300

    chore: init api

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,14 +1,29 @@
 // const path = require("path");
 module.exports = {
-  dialect: process.env.DIALECT,
-  host: process.env.HOST,
-  username: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-  operatorAliases: false,
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
+  Test: {
+    dialect: process.env.DIALECT,
+    host: process.env.HOST,
+    username: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    operatorAliases: false,
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
+  },
+  Production: {
+    dialect: process.env.DIALECT,
+    host: process.env.HOST,
+    username: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    operatorAliases: false,
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
   },
 };
