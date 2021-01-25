4858e2161f6c357726e36bf9dd66226acd2c5b93
leo-ventura30 <leonardo.ventura@outlook.com>
2021-01-25 20:04:39 -0300

    chore: reduce rules appointements

--- src/app/domains/AppointmentsDomain.js
+++ src/app/domains/AppointmentsDomain.js
@@ -4,35 +4,14 @@ const { Op } = require("sequelize");
 class AppointmentsDomains {
   async load(employers_id) {
     const allAppointments = await Appointment.findAll({
-      where: { [Op.and]: [{ employers_id, status: 1 }] },
+      where: { [Op.and]: { employers_id, status: 1 } },
     });
-    return allAppointments;
-    // var obj = [];
-
-    // allAppointments.forEach((element, key) => {
-    //   const { id, date, location, type, value, status, users_id } = element;
-
-    //   obj[key] = {
-    //     id,
-    //     date,
-    //     location,
-    //     type,
-    //     value,
-    //     status,
-    //     user: { users_id },
-    //   };
-    // });
-    // var count = 0;
-
-    // for (var appointment of obj) {
-    //   const {
-    //     user: { users_id },
-    //   } = appointment;
-    //   const allUsers = await UserDomain.load(users_id);
-    //   obj[count].user = allUsers;
-    //   count++;
-    // }
-    // return obj;
+    var appointments = [];
+    allAppointments.map((e, k) => {
+      const { id, date, location, type, value, status } = e;
+      appointments[k] = { id, date, location, type, value, status };
+    });
+    return { appointments };
   }
 
   async create(users_id, employers_id, appointment) {
