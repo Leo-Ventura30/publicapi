e2421068f45fb6d6be58c68fe1c13bedfdaa7aee
leo-ventura30 <leonardo.ventura@outlook.com>
2021-02-16 20:43:21 -0300

    chore: db config

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,20 +1,8 @@
 const dotenv = require("dotenv");
 dotenv.config();
 module.exports = {
-  development: {
-    dialect: process.env.DIALECTENV,
-    host: process.env.HOSTENV,
-    username: process.env.USERENV,
-    password: process.env.PASSWORDENV,
-    database: process.env.DATABASEENV,
-    operatorAliases: false,
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true,
-    },
-  },
   production: {
+    use_env_variable: process.env.USE_ENV_VARIABLE,
     dialect: process.env.DIALECT,
     host: process.env.HOST,
     username: process.env.USER,
