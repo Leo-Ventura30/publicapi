9d66f2890fedf2bbd4f58125b262a094af955412
leofventura98 <leonardo.ventura@outlook.com>
2021-08-31 22:51:03 -0300

    chore: fix jwt

--- src/app/middleware/auth.js
+++ src/app/middleware/auth.js
@@ -1,10 +1,13 @@
 const jwt = require("jsonwebtoken");
+const tmp_token = "438473874387"
+
 module.exports = (req, res, next) => {
   const token = req.headers["x-access-token"];
+
   if (!token)
     return res.status(401).json({ auth: false, error: "No token provided." });
 
-  jwt.verify(token, process.env.SECRET, function (err, decoded) {
+  jwt.verify(token, process.env.SECRET || tmp_token, function (err, decoded) {
     if (err) {
       if (err.message === "jwt expired")
         return res.json({ message: "Faça o login novamente!" });
--- src/app/services/jwt.js
+++ src/app/services/jwt.js
@@ -1,11 +1,12 @@
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
+const tmp_token = "438473874387"
 
 dotenv.config();
 
 class authToken {
   token(employers_id) {
-    return jwt.sign({ employers_id }, process.env.SECRET, {
+    return jwt.sign({ employers_id }, process.env.SECRET || tmp_token, {
       expiresIn: 60 * 60 * 24,
     });
   }
